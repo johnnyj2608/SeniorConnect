94e1987f2df251f12a8d02edea28ed3fb0441b97
Johnny Jiang <johnnyj2608@gmail.com>
2024-04-09 22:07:33 -0400

    Init commit

---
+++ SeniorConnect/SeniorConnect/SeniorConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeniorConnectApp.swift
+//  SeniorConnect
+//
+//  Created by Johnny Jiang on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SeniorConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
