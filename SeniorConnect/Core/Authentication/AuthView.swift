75dfdb1c55fe56a71c804fc397d629fc2de25ac2
Johnny Jiang <johnnyj2608@gmail.com>
2024-04-22 19:10:26 -0400

    Launch view animation

--- SeniorConnect/Core/Authentication/AuthView.swift
+++ SeniorConnect/Core/Authentication/AuthView.swift
@@ -15,48 +15,63 @@ struct AuthView: View {
     @StateObject private var viewModel = AuthViewModel()
     @Binding var showSignInView: Bool
     @State var currentNonce = ""
+    @State private var yOffset: CGFloat = 0
     
     var body: some View {
-        VStack {
-            Spacer()
-            GoogleSignInButton(viewModel: GoogleSignInButtonViewModel(scheme: .light,  style: .wide, state: .normal)) {
-                Task {
-                    do {
-                        try await viewModel.signInGoogle()
-                        showSignInView = false
-                    } catch {
-                        print(error)
+        ZStack {
+            VStack {
+                Spacer()
+                GoogleSignInButton(viewModel: GoogleSignInButtonViewModel(scheme: .light,  style: .wide, state: .normal)) {
+                    Task {
+                        do {
+                            try await viewModel.signInGoogle()
+                            showSignInView = false
+                        } catch {
+                            print(error)
+                        }
                     }
                 }
-            }
-            
-            SignInWithAppleButton { request in
-                let nonce = randomNonceString()
-                currentNonce = nonce
-                request.requestedScopes = [.email, .fullName]
-                request.nonce = sha256(nonce)
-            } onCompletion: { result in
-                Task {
-                    await viewModel.SignInApple(with: result, nonce: currentNonce)
-                    showSignInView = false
+                
+                SignInWithAppleButton { request in
+                    let nonce = randomNonceString()
+                    currentNonce = nonce
+                    request.requestedScopes = [.email, .fullName]
+                    request.nonce = sha256(nonce)
+                } onCompletion: { result in
+                    Task {
+                        await viewModel.SignInApple(with: result, nonce: currentNonce)
+                        showSignInView = false
+                    }
+                }
+                .frame(height: 40)
+                .cornerRadius(8)
+                
+                NavigationLink {
+                    EmailView(showSignInView: $showSignInView)
+                } label: {
+                    Text("Sign in with email")
+                        .font(.system(size: 18))
+                        .foregroundColor(.white)
+                        .frame(height: 40)
+                        .frame(maxWidth: .infinity)
+                        .background(Color(red: 0.7, green: 0.2, blue: 0.2))
+                        .cornerRadius(10)
                 }
             }
-            .frame(height: 40)
-            .cornerRadius(8)
-            
-            NavigationLink {
-                EmailView(showSignInView: $showSignInView)
-            } label: {
-                Text("Sign in with email")
-                    .font(.system(size: 18))
-                    .foregroundColor(.white)
-                    .frame(height: 40)
-                    .frame(maxWidth: .infinity)
-                    .background(Color(red: 0.7, green: 0.2, blue: 0.2))
-                    .cornerRadius(10)
+            .padding()
+            .background(Color(red: 111/255, green: 160/255, blue: 232/255))
+            Image("LOGIN")
+                .resizable()
+                .frame(width: 350, height: 350)
+                .offset(y: yOffset)
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                withAnimation(.easeInOut(duration: 1.0)) {
+                    yOffset = -150
+                }
             }
         }
-        .padding()
-        .background(Color(red: 111/255, green: 160/255, blue: 232/255))
     }
+    
 }
---
+++ SeniorConnect/Extensions/Color.swift
@@ -0,0 +1,18 @@
+//
+//  Color.swift
+//  SeniorConnect
+//
+//  Created by Johnny Jiang on 4/22/24.
+//
+
+import SwiftUI
+
+extension Color {
+    static let launch = LaunchTheme()
+}
+
+struct LaunchTheme {
+    
+    let accent = Color("LaunchAccentColor")
+    let background = Color("LaunchBackgroundColor")
+}
