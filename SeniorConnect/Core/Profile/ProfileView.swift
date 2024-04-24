8c2a1ab80857ac255398c6e034e5003d33629d95
Johnny Jiang <johnnyj2608@gmail.com>
2024-04-23 23:34:09 -0400

    Profile view section skeleton

--- SeniorConnect/Core/Profile/ProfileView.swift
+++ SeniorConnect/Core/Profile/ProfileView.swift
@@ -21,84 +21,128 @@ struct ProfileView: View {
     }
     
     var body: some View {
-        List {
-            if let user = viewModel.user {
-                Text("UserId: \(user.userId)")
-                Text("Email: \(user.email ?? "")")
-                
-                VStack {
-                    HStack {
-                        ForEach(languages, id: \.self) { string in
-                            Button(string) {
-                                if languageSelected(text: string) {
-                                    viewModel.removeLanguagePreference(preference: string)
-                                } else {
-                                    viewModel.addLanguagePreference(preference: string)
-                                }
-                            }
-                            .font(.headline)
-                            .buttonStyle(.borderedProminent)
-                            .tint(languageSelected(text: string) ? .green : .red)
-                        }
-                    }
-                    Text("Language Preference:")
-                    Text("\((user.preferenceLanguage ?? [" "]).joined(separator: ", "))")
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                }
-                Button {
-                    if user.favoriteActivity == nil {
-                        viewModel.addFavoriteActivity(activity: Activity(id: "1", name: "Dancing", support: false))
-                    } else {
-                        viewModel.removeFavoriteActivity()
-                    }
-                } label: {
-                    Text("Favorite Activity: \(user.favoriteActivity?.name ?? "")")
-                }
-                PhotosPicker(
-                    selection: $selectedItem,
-                    matching: .images,
-                    photoLibrary: .shared()) {
-                        Text("Selected a photo")
-                    }
-                if let urlString = viewModel.user?.profileImagePathUrl, let url = URL(string: urlString) {
-                    AsyncImage(url: url) { image in
-                        image
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 150, height: 150)
-                            .cornerRadius(10)
-                    } placeholder: {
-                        ProgressView()
-                            .frame(width: 150, height: 150)
-                    }
-                }
-
-                if viewModel.user?.profileImagePathUrl != nil {
-                    Button("Delete image") {
-                        viewModel.deleteProfileImage()
-                    }
-                }
-            }
-        }
-        .task {
-            try? await viewModel.loadCurrentUser()
-        }
-        .onChange(of: selectedItem, perform: { newValue in
-            if let newValue {
-                viewModel.saveProfileImage(item: newValue)
-            }
-        })
-        .navigationTitle("Profile")
-        .toolbar {
-            ToolbarItem(placement: .navigationBarTrailing) {
-                NavigationLink {
-                    SettingsView(showSignInView: $showSignInView)
-                } label: {
-                    Image(systemName: "gear")
-                        .font(.headline)
+        NavigationStack {
+            Form {
+                Section(header: Text("User")) {
+                    // User information fields
+                    // profile pic, name, dob, gender, address, note
                 }
                 
+                Section(header: Text("Day care")) {
+                    // Day care information fields
+                    // name, address, phone, fax, hours
+                }
+                
+                Section(header: Text("Insurance")) {
+                    // Insurance information fields
+                    // name, schedule, transport, medicaid
+                }
+                
+                Section(header: Text("Contact")) {
+                    // Contact information fields
+                    // phone, email, EC, PCP, CM, HA
+                }
+                
+                Section(header: Text("Preferences")) {
+                    // Preferences information fields
+                    // Employee (M/F), Nutrition (Diet), Allergies (Severity),
+                    // Goals, Risks, Language, Functionals
+                }
+                
+                Section(header: Text("App Settings")) {
+                    // App settings toggles
+                    // Dark mode, notifications
+                }
+                
+                Section(header: Text("Feedback")) {
+                    // Feedback share link, rate app link
+                }
+                
+                Section(header: Text("Account")) {
+                    // Account actions
+                    // Logout, delete
+                }
             }
+            .navigationBarTitle("Profile")
         }
+        //        List {
+        //            if let user = viewModel.user {
+        //                Text("UserId: \(user.userId)")
+        //                Text("Email: \(user.email ?? "")")
+        //
+        //                VStack {
+        //                    HStack {
+        //                        ForEach(languages, id: \.self) { string in
+        //                            Button(string) {
+        //                                if languageSelected(text: string) {
+        //                                    viewModel.removeLanguagePreference(preference: string)
+        //                                } else {
+        //                                    viewModel.addLanguagePreference(preference: string)
+        //                                }
+        //                            }
+        //                            .font(.headline)
+        //                            .buttonStyle(.borderedProminent)
+        //                            .tint(languageSelected(text: string) ? .green : .red)
+        //                        }
+        //                    }
+        //                    Text("Language Preference:")
+        //                    Text("\((user.preferenceLanguage ?? [" "]).joined(separator: ", "))")
+        //                        .frame(maxWidth: .infinity, alignment: .leading)
+        //                }
+        //                Button {
+        //                    if user.favoriteActivity == nil {
+        //                        viewModel.addFavoriteActivity(activity: Activity(id: "1", name: "Dancing", support: false))
+        //                    } else {
+        //                        viewModel.removeFavoriteActivity()
+        //                    }
+        //                } label: {
+        //                    Text("Favorite Activity: \(user.favoriteActivity?.name ?? "")")
+        //                }
+        //                PhotosPicker(
+        //                    selection: $selectedItem,
+        //                    matching: .images,
+        //                    photoLibrary: .shared()) {
+        //                        Text("Selected a photo")
+        //                    }
+        //                if let urlString = viewModel.user?.profileImagePathUrl, let url = URL(string: urlString) {
+        //                    AsyncImage(url: url) { image in
+        //                        image
+        //                            .resizable()
+        //                            .scaledToFill()
+        //                            .frame(width: 150, height: 150)
+        //                            .cornerRadius(10)
+        //                    } placeholder: {
+        //                        ProgressView()
+        //                            .frame(width: 150, height: 150)
+        //                    }
+        //                }
+        //
+        //                if viewModel.user?.profileImagePathUrl != nil {
+        //                    Button("Delete image") {
+        //                        viewModel.deleteProfileImage()
+        //                    }
+        //                }
+        //            }
+        //        }
+        //        .task {
+        //            try? await viewModel.loadCurrentUser()
+        //        }
+        //        .onChange(of: selectedItem, perform: { newValue in
+        //            if let newValue {
+        //                viewModel.saveProfileImage(item: newValue)
+        //            }
+        //        })
+        //        .navigationTitle("Profile")
+        //        .toolbar {
+        //            ToolbarItem(placement: .navigationBarTrailing) {
+        //                NavigationLink {
+        //                    SettingsView(showSignInView: $showSignInView)
+        //                } label: {
+        //                    Image(systemName: "gear")
+        //                        .font(.headline)
+        //                }
+        //
+        //            }
+        //        }
     }
 }
