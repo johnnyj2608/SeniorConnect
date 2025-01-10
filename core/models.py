2f8291b9af16ddf29d50a1e633030ce85eec86a1
Johnny Jiang <johnnyj2608@gmail.com>
2025-01-10 18:40:47 -0500

    Members model draft

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Member(models.Model):
+    first_name = models.CharField(max_length=255, null=False, blank=False)
+    last_name = models.CharField(max_length=255, null=False, blank=False)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
--- core/views.py
+++ core/views.py
@@ -4,4 +4,38 @@ from django.http import JsonResponse
 # Create your views here.
 
 def getRoutes(request):
-    return JsonResponse('Our API', safe=False)
+
+    routes = [
+        {
+            'Endpoint': '/members/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns an array of members'
+        },
+        {
+            'Endpoint': '/members/id',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns a single member object'
+        },
+        {
+            'Endpoint': '/members/create/',
+            'method': 'POST',
+            'body': {'body': ""},
+            'description': 'Creates new member with data sent in post request'
+        },
+        {
+            'Endpoint': '/members/id/update/',
+            'method': 'PUT',
+            'body': {'body': ""},
+            'description': 'Creates an existing member with data sent in post request'
+        },
+        {
+            'Endpoint': '/members/id/delete/',
+            'method': 'DELETE',
+            'body': None,
+            'description': 'Deletes and exiting member'
+        },
+    ]
+
+    return JsonResponse(routes, safe=False)
