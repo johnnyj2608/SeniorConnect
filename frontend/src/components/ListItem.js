26969cfef645ae83b0ff56031fc2c56d49476f9d
Johnny Jiang <johnnyj2608@gmail.com>
2025-01-13 12:42:27 -0500

    Last updated subtitle

--- frontend/src/components/ListItem.js
+++ frontend/src/components/ListItem.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+let getTime = (member) => {
+  return new Date(member.updated).toLocaleDateString()
+}
+
 const ListItem = ({member}) => {
   return (
     <Link to={`/member/${member.id}`}>
       <div className="members-list-item">
         <h3>{member.first_name} {member.last_name}</h3>
+        <p><span>{getTime(member)}</span></p>
       </div>
     </Link>
   )
