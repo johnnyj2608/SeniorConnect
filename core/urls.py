943df73a99e4d483d25ebcc11b449bb151737e7e
Johnny Jiang <johnnyj2608@gmail.com>
2025-01-12 19:34:29 -0500

    Django Rest Framework Init

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getRoutes, name="routes")
+    path('', views.getRoutes, name="routes"),
+    path('members/', views.getMembers, name="members"),
+    path('members/<str:pk>/', views.getMember, name="member"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Member
+from .serializers import MemberSerializer
 
 # Create your views here.
 
+@api_view(['GET'])
 def getRoutes(request):
 
     routes = [
@@ -38,4 +42,16 @@ def getRoutes(request):
         },
     ]
 
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
+
+@api_view(['GET'])
+def getMembers(request):
+    members = Member.objects.all()
+    serializer = MemberSerializer(members, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getMember(request, pk):
+    member = Member.objects.get(id=pk)
+    serializer = MemberSerializer(member, many=False)
+    return Response(serializer.data)
