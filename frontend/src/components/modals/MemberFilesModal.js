341c8483c39872a4886450c61d0c98917a945d40
Johnny Jiang <johnnyj2608@gmail.com>
2025-05-04 23:27:19 -0400

    Drag and drop file for upload

--- frontend/src/components/modals/MemberFilesModal.js
+++ frontend/src/components/modals/MemberFilesModal.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { ReactComponent as Eye } from '../../assets/eye.svg'
 import { ReactComponent as Trash } from '../../assets/trash.svg'
 import { ReactComponent as Add } from '../../assets/add.svg'
+import { ReactComponent as Upload } from '../../assets/upload.svg'
 import viewFile from '../../utils/viewFile';
 import { formatDate } from '../../utils/formatUtils';
 import Dropdown from '../inputs/Dropdown';
@@ -12,6 +13,8 @@ const MemberFilesModal = ({ data, handleChange, activeTab }) => {
 
     const [versionIndex, setVersionIndex] = useState(0);
     const currentVersion = versions[versionIndex] || {};
+
+    const [isDragging, setIsDragging] = useState(false);
 
     const disabled = data.filter(tab => !tab.deleted).length <= 0;
     const disabledVersions = disabled || versions.length <= 0;
@@ -58,115 +61,152 @@ const MemberFilesModal = ({ data, handleChange, activeTab }) => {
         setVersionIndex(0);
     };
 
+    const handleDrop = (e) => {
+        e.preventDefault();
+        setIsDragging(false);
+
+        const file = e.dataTransfer.files[0];
+        if (file && file.type === 'application/pdf') {
+            const fakeEvent = {
+                target: {
+                    files: [file]
+                }
+            };
+            handleChange('file', true, versionIndex)(fakeEvent);
+        }
+    };
+
+    const handleDragOver = (e) => {
+        e.preventDefault();
+        setIsDragging(true);
+    };
+
+    const handleDragLeave = () => {
+        setIsDragging(false);
+    };
+
     useEffect(() => {
         setVersionIndex(0);
     }, [activeTab]);
 
     const versionOptions = versions.map((version, index) => ({
-        name: `Version ${versions.length - index} - Uploaded ${formatDate(version.uploaded_at) || 'N/A'}`,
+        name: `V${versions.length - index} - Uploaded ${formatDate(version.uploaded_at) || 'N/A'}`,
         value: index
     }));
 
     return (
-        <>
-            <h3>Edit Files</h3>
-            <div className="member-detail">
-                <label>Name *</label>
-                <input
-                    type="text"
-                    value={disabled ? '' : current.name || ''}
-                    onChange={handleChange('name')}
-                    autoComplete="off"
-                    disabled={disabled}
-                />
-            </div>
-            <div className="member-detail">
-                <label>Upload File *</label>
-                <div className="file-container">
-                    <button
-                        className="custom-file-button"
-                        onClick={() => document.getElementById('hiddenFileInput').click()}
-                        disabled={disabledVersions}
-                    >
-                        Choose File
-                    </button>
-                    <span className={`uploaded-file-name ${!currentVersion.file && !disabledVersions ? 'no-file-chosen' : ''}`}>
-                        {currentVersion.file ? 'File chosen' : 'No file chosen'}
-                    </span>
+        <div
+            className={`file-drop ${isDragging ? 'drag-over' : ''}`}
+            onDrop={handleDrop}
+            onDragOver={handleDragOver}
+            onDragLeave={handleDragLeave}
+        >
+            <div className={`file-content ${isDragging ? 'dimmed' : ''}`}>
+                <h3>Edit Files</h3>
+                <div className="member-detail">
+                    <label>Name *</label>
                     <input
-                        id="hiddenFileInput"
-                        type="file"
-                        accept="application/pdf"
-                        onChange={handleChange('file', true, versionIndex)}
-                        style={{ display: 'none' }}
+                        type="text"
+                        value={disabled ? '' : current.name || ''}
+                        onChange={handleChange('name')}
+                        autoComplete="off"
+                        disabled={disabled}
                     />
                 </div>
-            </div>
-            <div className="member-detail">
-                <label>Completed</label>
-                <input
-                    type="date"
-                    value={disabled ? '' : currentVersion.completion_date || ''}
-                    onChange={handleChange('completion_date', true, versionIndex)}
-                    disabled={disabledVersions}
-                />
-            </div>
-            <div className="member-detail">
-                <label>Expiration</label>
-                <input
-                    type="date"
-                    value={disabled ? '' : currentVersion.expiration_date || ''}
-                    onChange={handleChange('expiration_date', true, versionIndex)}
-                    disabled={disabledVersions}
-                />
-            </div>
+                <div className="member-detail">
+                    <label>Upload File *</label>
+                    <div className="file-container">
+                        <button
+                            className="custom-file-button"
+                            onClick={() => document.getElementById('hiddenFileInput').click()}
+                            disabled={disabledVersions}
+                        >
+                            Choose File
+                        </button>
+                        <span className={`uploaded-file-name ${!currentVersion.file && !disabledVersions ? 'no-file-chosen' : ''}`}>
+                            {currentVersion.file ? 'File chosen' : 'No file chosen'}
+                        </span>
+                        <input
+                            id="hiddenFileInput"
+                            type="file"
+                            accept="application/pdf"
+                            onChange={handleChange('file', true, versionIndex)}
+                            style={{ display: 'none' }}
+                        />
+                    </div>
+                </div>
+                <div className="member-detail">
+                    <label>Completed</label>
+                    <input
+                        type="date"
+                        value={disabled ? '' : currentVersion.completion_date || ''}
+                        onChange={handleChange('completion_date', true, versionIndex)}
+                        disabled={disabledVersions}
+                    />
+                </div>
+                <div className="member-detail">
+                    <label>Expiration</label>
+                    <input
+                        type="date"
+                        value={disabled ? '' : currentVersion.expiration_date || ''}
+                        onChange={handleChange('expiration_date', true, versionIndex)}
+                        disabled={disabledVersions}
+                    />
+                </div>
 
-            <div className="member-detail">
-                <label>Versions</label>
-                <Dropdown
-                    display={versionOptions[versionIndex]?.name || ''}
-                    onChange={(e) => setVersionIndex(Number(e.target.value))}
-                    options={versionOptions}
-                    disabled={disabledVersions}
-                    placeholder={false}
-                />
-            </div>
+                <div className="member-detail">
+                    <label>Versions</label>
+                    <Dropdown
+                        display={versionOptions[versionIndex]?.name || ''}
+                        onChange={(e) => setVersionIndex(Number(e.target.value))}
+                        options={versionOptions}
+                        disabled={disabledVersions}
+                        placeholder={false}
+                    />
+                </div>
 
-            <div className="file-buttons">
-                <button 
-                    className={`arrow-btn add-btn tooltip ${versions.length === 0 ? 'pulse' : ''}`} 
-                    onClick={handleAdd}
-                    data-tooltip="Add new version"
-                >
-                    <Add />
-                </button>
-                <button 
-                    className="arrow-btn tooltip"
-                    onClick={() => viewFile(currentVersion.file)}
-                    disabled={!currentVersion.file}
-                    data-tooltip="View version"
-                >
-                    <Eye />
-                </button>
-                <button 
-                    className="arrow-btn tooltip trash-can"
-                    onClick={handleDelete}
-                    disabled={disabledVersions}
-                    data-tooltip="Delete version"
-                >
-                    <Trash />
-                </button>
+                <div className="file-buttons">
+                    <button 
+                        className={`arrow-btn add-btn tooltip ${versions.length === 0 ? 'pulse' : ''}`} 
+                        onClick={handleAdd}
+                        data-tooltip="Add new version"
+                    >
+                        <Add />
+                    </button>
+                    <button 
+                        className="arrow-btn tooltip"
+                        onClick={() => viewFile(currentVersion.file)}
+                        disabled={!currentVersion.file}
+                        data-tooltip="View version"
+                    >
+                        <Eye />
+                    </button>
+                    <button 
+                        className="arrow-btn tooltip trash-can"
+                        onClick={handleDelete}
+                        disabled={disabledVersions}
+                        data-tooltip="Delete version"
+                    >
+                        <Trash />
+                    </button>
+                </div>
+                <div className="file-footer">
+                    {versions.length === 0 ? (
+                        <p>No files available. Please add a file.</p>
+                    ) : (
+                        <p>
+                            File {versionIndex + 1} of {versions.length}
+                        </p>
+                    )}
+                </div>
             </div>
-            <div className="file-footer">
-                {versions.length === 0 ? (
-                    <p>No files available. Please add a file.</p>
-                ) : (
-                    <p>
-                        File {versionIndex + 1} of {versions.length}
-                    </p>
-                )}
-            </div>
-        </>
+            {isDragging && (
+                <div className="upload-overlay">
+                    <Upload className="upload-icon pulse" />
+                    <p>Drop PDF to upload</p>
+                </div>
+            )}
+        </div>
     )
 }
 
