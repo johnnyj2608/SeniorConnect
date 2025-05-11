61b27bc225ba3c0d88fec6fa748e99d8f20bd1e4
Johnny Jiang <johnnyj2608@gmail.com>
2025-05-11 13:57:09 -0400

    Fixed capitalization of renamed files

---
+++ frontend/src/components/homeCards/HomeAuthCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HomeAuthCard = () => {
+  return (
+    <div className="home-section">
+        <h3>Authorizations</h3>
+        <ul>
+            <li>Jane's authorization expires on 4/1/25</li>
+        </ul>
+    </div>
+  )
+}
+
+export default HomeAuthCard
