7e688930e9dca83535180809b7c1f16930fc1ef9
Johnny Jiang <johnnyj2608@gmail.com>
2025-05-23 14:57:07 -0400

    Loading screen styling

--- frontend/src/components/auth/PrivateRoute.js
+++ frontend/src/components/auth/PrivateRoute.js
@@ -5,7 +5,13 @@ import { AuthContext } from '../../context/AuthContext'
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext)
 
-  if (loading) return <div>Loading...</div>
+  if (loading) 
+    return (
+      <div className="loading-container">
+        <div className="spinner"></div>
+        <h2>Loading...</h2>
+      </div>
+    )
 
   return user ? children : <Navigate to="/login" replace />
 }
