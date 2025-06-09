deb7431cbd22f1bada1cf2748bb2fad1bde77382
Johnny Jiang <johnnyj2608@gmail.com>
2025-06-09 10:34:20 -0400

    Hide contact if missing name and phone

--- frontend/src/components/layout/ContactDetail.js
+++ frontend/src/components/layout/ContactDetail.js
@@ -2,18 +2,15 @@ import React from 'react'
 import { formatPhone } from '../../utils/formatUtils';
 
 const ContactDetail = ({ label, name, phone }) => {
+    if (name == null || name === '') return null;
+    if (phone == null || phone === '') return null;
+
     return (
         <div className="member-detail">
             <label>{label}:</label>
             <span className="member-detail-contacts">
-                {(name || phone) ? (
-                    <>
-                        <div>{name}</div>
-                        <div>{formatPhone(phone)}</div>
-                    </>
-                ) : (
-                    'N/A'
-                )}
+                <div>{name}</div>
+                <div>{formatPhone(phone)}</div>
             </span>
         </div>
     );
