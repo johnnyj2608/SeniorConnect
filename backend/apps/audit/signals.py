7f974e79b260fa4ed9e5623526f428ddc2d2115f
Johnny Jiang <johnnyj2608@gmail.com>
2025-09-18 10:55:55 -0400

    Remove mltc changes from user audit log

--- backend/apps/audit/signals.py
+++ backend/apps/audit/signals.py
@@ -11,9 +11,8 @@ from ..core.models.authorization_model import Authorization
 from ..core.models.absence_model import Absence
 from ..core.models.file_model import File
 from ..core.models.gifted_model import Gifted
-from ..tenant.models.mltc_model import Mltc
 
-WHITELISTED_MODELS = (Member, Contact, Authorization, Mltc, Absence, File, Gifted)
+WHITELISTED_MODELS = (Member, Contact, Authorization, Absence, File, Gifted)
 
 def get_related_member(instance):
     if hasattr(instance, '_acting_member'):
